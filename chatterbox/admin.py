5f18754c55b924ef29cc42430b52ef20eaf08f7c
Marky Schwoiser <markyschwoiser@mpmp-MBP.home>
2022-09-07 21:06:38 +0200

    Pridan Room a Message model

--- chatterbox/admin.py
+++ chatterbox/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
+from chatterbox.models import Room, Message
+
 # Register your models here.
+admin.site.register(Room)
+admin.site.register(Message)
+
+
+
+
+
+
+
--- chatterbox/views.py
+++ chatterbox/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
 
 # Create your views here.
-def hello(request):
-    return HttpResponse('Hello World!!!')
+def hello(request, s):
+    return HttpResponse(f'Hello, {s} world!')
 
